3bea2410dd913e78df1f02a91b69ffc81a99cc33
Jianbo Ye <jxy198@ist.psu.edu>
2016-10-24 22:18:52 -0400

    some updates

--- d2suite/src/common/d2_badmm.hpp
+++ d2suite/src/common/d2_badmm.hpp
@@ -10,14 +10,37 @@
 namespace d2 {
 #define eps (1E-16)
   struct BADMMCache {
+    real_t *C;
+    real_t *Ctmp;
+    real_t *Pi1;
+    real_t *Pi2;
+    real_t *Lambda;
+    real_t *Ltmp;
+    real_t *buffer;
   };
 
   template <typename ElemType1, typename ElemType2>
   void allocate_badmm_cache(const ElemType1 &a, const ElemType2 &b,
 			    BADMMCache &cache) {
+    const size_t n = a.len * b.len;
+    const size_t m = a.len + b.len;
+    cache.C      = new real_t[n];
+    cache.Ctmp   = new real_t[n];
+    cache.Pi1    = new real_t[n];
+    cache.Pi2    = new real_t[n];
+    cache.Lambda = new real_t[n];
+    cache.Ltmp   = new real_t[n];
+    cache.buffer = new real_t[m];
   }
 
   void deallocate_badmm_cache(BADMMCache &cache) {
+    delete [] cache.C;
+    delete [] cache.Ctmp;
+    delete [] cache.Pi1;
+    delete [] cache.Pi2;
+    delete [] cache.Lambda;
+    delete [] cache.Ltmp;
+    delete [] cache.buffer;
   }
 
 
@@ -25,6 +48,23 @@ namespace d2 {
   int EMD_BADMM(const ElemType1 &a, const ElemType2 &b,
 		const BADMMCache &cache,
 		const size_t niter) {
+    const size_t mat_size = a.len * b.len;
+    for (size_t i=0; i<mat_size; ++i)
+      cache.Ctmp[i] = exp(cache.C[i]);
+    for (size_t iter=0; iter < niter; ++iter) {
+      for (size_t i=0; i<mat_size; ++i)
+	cache.Ltmp[i] = exp(cache.Lambda[i]);
+      for (size_t i=0; i<mat_size; ++i)
+	cache.Pi1[i] = cache.Pi2[i] / (cache.Ctmp[i] * cache.Ltmp[i]) + eps;
+      _D2_FUNC(rnorm)(a.len, b.len, cache.Pi1, cache.buffer);
+      _D2_FUNC(gcms)(a.len, b.len, cache.Pi1, a.w);
+      for (size_t i=0; i<mat_size; ++i)
+	cache.Pi2[i] = cache.Pi1[i] * cache.Ltmp[i] + eps;
+      _D2_FUNC(cnorm)(a.len, b.len, cache.Pi2, cache.buffer);
+      _D2_FUNC(grms)(a.len, b.len, cache.Pi2, b.w);
+      for (size_t i=0; i<mat_size; ++i)
+	cache.Lambda[i] = cache.Lambda[i] + cache.Pi1[i] - cache.Pi2[i];
+    }
     return 0;
   }
 
--- d2suite/src/learn/marriage_learner.hpp
+++ d2suite/src/learn/marriage_learner.hpp
@@ -10,14 +10,34 @@ namespace d2 {
   template <typename ElemType1, typename FuncType, size_t dim>
   void ML_BADMM (Block<ElemType1> &data,
 		 Elem<def::Function<FuncType>, dim> &learner,
-		 const size_t max_iter) {
+		 const size_t max_iter,
+		 const real_t rho = 2.0) {    
     BADMMCache *badmm_cache_arr = new BADMMCache[data.get_size()];
+    
     for (size_t i=0; i<data.get_size(); ++i)
       allocate_badmm_cache(data[i], learner, badmm_cache_arr[i]);
 
+
+    for (size_t i=0; i<data.get_size(); ++i) {
+      // initialize badmm_cache_arr[i].{Pi2, Lambda}
+    }
+    
     for (size_t iter=0; iter < max_iter; ++iter) {
-      for (size_t i=0; i<data.get_size();++i)
-	EMD_BADMM(data[i], learner, badmm_cache_arr[i],1);
+      real_t totalC = 0;
+      for (size_t i=0; i<data.get_size();++i) {
+	_pdist2(data[i].supp, data[i].label, data[i].len,
+		learner.supp, learner.len,
+		data.meta, badmm_cache_arr[i].C);
+	totalC += _D2_CBLAS_FUNC(asum)(data[i].len * learner.len, badmm_cache_arr[i].C, 1);
+      }
+      totalC /= (data.get_col() * learner.len);
+      for (size_t i=0; i<data.get_size();++i) {
+	for (size_t j=0; j<data[i].len * learner.len; ++j)
+	  badmm_cache_arr[i].C[j] /= (totalC*rho);
+      }
+      for (size_t i=0; i<data.get_size();++i) {
+	EMD_BADMM(data[i], learner, badmm_cache_arr[i],1);	
+      }
     }
     
     for (size_t i=0; i<data.get_size(); ++i)
