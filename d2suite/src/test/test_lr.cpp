087c75f95cb9eeb521a228e9d3c2dcba7e9a3ec4
Jianbo Ye <jxy198@ist.psu.edu>
2016-10-27 00:52:13 -0400

    better unit test

--- d2suite/src/test/test_lr.cpp
+++ d2suite/src/test/test_lr.cpp
@@ -4,7 +4,7 @@
 #define D 10
 using namespace d2;
 
-void sample_naive_data(real_t *X, real_t *y) {
+void sample_naive_data(real_t *X, real_t *y, real_t *w) {
   for (size_t i=0; i<N; ++i) {
     y[i] = rand() % 2;
     if (y[i]) {
@@ -12,8 +12,9 @@ void sample_naive_data(real_t *X, real_t *y) {
 	X[i*D+j]=(real_t) rand() / (real_t) RAND_MAX;
     } else {
       for (size_t j=0; j<D; ++j)
-	X[i*D+j]=(real_t) rand() / (real_t) RAND_MAX - 1.;
+	X[i*D+j]=(real_t) rand() / (real_t) RAND_MAX - .1;
     }
+    w[i] = 1.; // (real_t) rand() / (real_t) RAND_MAX;
   }  
 }
 
@@ -27,12 +28,13 @@ real_t accuracy(real_t *y_pred, real_t *y_true, size_t n) {
 int main() {
   real_t *X = new real_t[D*N];
   real_t *y = new real_t[N];
+  real_t *w = new real_t[N];
   real_t *y_pred = new real_t[N];
-  sample_naive_data(X, y);
+  sample_naive_data(X, y, w);
   auto classifier = new Logistic_Regression<D, 2>();
   classifier->init();
-  classifier->fit(X, y, NULL, N);
-  sample_naive_data(X, y);
+  classifier->fit(X, y, w, N);
+  sample_naive_data(X, y, w);
   classifier->predict(X, N, y_pred);
   printf("accuracy: %.3f\n", accuracy(y_pred, y, N) );  
   return 0;
